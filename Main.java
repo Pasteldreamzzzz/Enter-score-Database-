efd8a16a498ecb878b53842a1dd8007e01ede9c8
Pasteldreamzzzz <gabrielle.paul@patelhs.org>
2020-10-16 20:20:16 +0000

    Determined what makes you win the game or lose

--- Main.java
+++ Main.java
@@ -8,11 +8,16 @@ class Main {
 
 
     // Enter the score: The game point system only goes up to 20 
+    
     int s = scores.nextInt();
     System.out.println(s);
-
     
-      
+    if (s > 1 && s <= 10 ){
+      System.out.println("Game lost!");
+    } else if (s >= 11 && s< 20){
+      System.out.println("Game won!");
+
+    // If you have less than 11 points at the end of the game you lose 
     }
   }
 }
