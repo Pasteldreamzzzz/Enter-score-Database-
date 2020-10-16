c9c5cab2bbb4994fdbb5bc671b36350f2fd152e8
Pasteldreamzzzz <gabrielle.paul@patelhs.org>
2020-10-16 20:12:13 +0000

    Added the Int

--- Main.java
+++ Main.java
@@ -4,5 +4,15 @@ class Main {
  
   public static void main(String[] args) {
     Scanner scores = new Scanner(System.in);
+    System.out.println("Enter the score:");
+
+
+    // Enter the score: The game point system only goes up to 20 
+    int s = scores.nextInt();
+    System.out.println(s);
+
+    
+      
+    }
   }
 }
