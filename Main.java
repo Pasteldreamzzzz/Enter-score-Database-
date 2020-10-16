91160646bb4202a3bffcc0512cd7bd473d56a96a
Pasteldreamzzzz <gabrielle.paul@patelhs.org>
2020-10-16 20:33:01 +0000

    Cleaned up the code spaced out the line to make it easier read

--- Main.java
+++ Main.java
@@ -10,17 +10,23 @@ class Main {
     // Enter the score: The game point system only goes up to 20 
     
     int s = scores.nextInt();
+    
     System.out.println(s);
     
     if (s > 1 && s <= 10 ){
+     
       System.out.println("Game lost!");
-    } else if (s >= 11 && s< 20){
+    }
+     else if (s >= 11 && s< 20){
+     
       System.out.println("Game won!");
 
     // If you have less than 11 points at the end of the game you lose
 
     } else {
+      
       System.out.println("The score you entered is not applicable");
+     
       //For people who try to cheat and put scores that cannot count 
     } 
   }
